ae64b604a6df30de11c5d25d0a4b6a36a7f17625
Luis Tapia <86934099+tapilew@users.noreply.github.com>
2024-07-24 11:53:40 -0500

    fix: use Marked as parser lib
    Nested lists didn't render correctly.

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
-// Showdown JS
-const showdown = require("showdown");
-const converter = new showdown.Converter();
+// Marked
+import { marked } from "marked";
 
-// Files
 const markdownFile = Bun.file("README.md");
 const markdownText = await markdownFile.text();
-const htmlContent = converter.makeHtml(markdownText);
+// Promise type handler
+const parsedHtml = marked.parse(markdownText);
+let html = "";
+if (typeof parsedHtml == "string") {
+  html = parsedHtml;
+}
+
+// Create or clean file
 const index = Bun.file("index.html");
+await Bun.write(index, "");
+// Writer
 const writer = index.writer();
 writer.write(`<!DOCTYPE html>
     <html Lang="es">
@@ -19,6 +26,6 @@ writer.write(`<!DOCTYPE html>
     <body>
     <main>
     \n`);
-writer.write(htmlContent);
+writer.write(html);
 writer.write(`\n</main>\n</body>\n</html>`);
 writer.end();
